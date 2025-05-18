a1fa19738303e1ba0a787188528e519c5d8db672
Paul Burke <paul@paulburke.co>
2025-05-17 20:06:59 -0400

    feat: adds user creation api

--- packages/web/src/app/api/user/route.ts
+++ packages/web/src/app/api/user/route.ts
@@ -1,9 +1,14 @@
-import { jwtVerify, createRemoteJWKSet } from "jose";
-import { lensClient } from "@/lib/lens/server";
+import { createRemoteJWKSet, jwtVerify } from "jose";
 import supabase from "@/lib/supabase/admin";
+import publicClient from "@/lib/viem/publicClient";
+import { accountAbi } from "@/lib/abis/account";
+import { graphApi } from "@/lib/abis/graph";
+import { getContract } from "@/lib/lens/contracts";
+import { fameishAbi } from "@/lib/abis/fameish";
+import { fetchAccount } from "@lens-protocol/client/actions";
+import { lensClient } from "@/lib/lens/server";
 
 const jwksUri = process.env.LENS_JWKS_URI!;
-const JWKS = createRemoteJWKSet(new URL(jwksUri));
 
 export async function POST(req: Request) {
   const token = req.headers.get("authorization")?.split(" ")[1];
@@ -15,25 +20,151 @@ export async function POST(req: Request) {
   }
 
   const body = await req.json();
-  const { account } = body;
+  console.log("POST /user : body", body);
+  const accountAddress = body.account as `0x${string}`;
+  if (!accountAddress) {
+    return new Response("Invalid account", {
+      status: 400,
+    });
+  }
 
   try {
-    // Verify the JWT using the JWKS
+    const JWKS = createRemoteJWKSet(new URL(jwksUri));
     const { payload } = await jwtVerify(token, JWKS);
-    const { sub: signer, act: account } = payload;
+    console.log("POST /user : JWT payload", payload);
+    const { sub: signer, act } = payload;
 
-    // TODO Check if the signer is a manager or owner of the account
+    if (act && typeof act === "string" && accountAddress.toLowerCase() !== act.toLowerCase()) {
+      throw new Error("JWT verification failed"); // returns 401 Unauthorized
+    }
 
-    // TODO Check if the account already exists in the database
-    // const userQuery = supabase
-    //   .from("users")
+    console.log("POST /user : ✓ JWT verified");
 
-    // TODO Create the account in the database
+    // check that the account has a score above the threshold
+    const getAccountRes = await fetchAccount(lensClient, {
+      address: accountAddress,
+    });
+    if (getAccountRes.isErr()) {
+      console.error("POST /user : getAccountRes", getAccountRes.error);
+      return new Response("Failed to fetch account", {
+        status: 500,
+      });
+    }
+    const account = getAccountRes.value;
+    if (!account || account.score < Number(process.env.LENS_MIN_ACCOUNT_SCORE!)) {
+      return new Response("Account score is too low", {
+        status: 401,
+      });
+    }
+
+    console.log("POST /user : ✓ Score verified");
+
+    // const isSignerOwnerOrManager = await publicClient.readContract({
+    //   address: account as `0x${string}`,
+    //   abi: accountAbi,
+    //   functionName: "canExecuteTransactions",
+    //   args: [signer as `0x${string}`],
+    // });
+
+    const canExecuteTransactions = await publicClient.multicall({
+      contracts: [
+        {
+          address: accountAddress,
+          abi: accountAbi,
+          functionName: "canExecuteTransactions",
+          args: [signer as `0x${string}`],
+        },
+        {
+          address: accountAddress,
+          abi: accountAbi,
+          functionName: "canExecuteTransactions",
+          args: [process.env.LENS_ACCOUNT_MANAGER_ADDRESS! as `0x${string}`],
+        },
+      ],
+    });
+
+    // Check if the signer is the owner or the manager of the account
+    if (canExecuteTransactions[0].error || !canExecuteTransactions[0].result) {
+      throw new Error("Signer is not a manager"); // returns 401 Unauthorized
+    }
+
+    // Check if the Fameish Account Manager is a manager of the account
+    if (canExecuteTransactions[1].error || !canExecuteTransactions[1].result) {
+      return new Response("Fameish is not a manager of the account", {
+        status: 401,
+      });
+    }
+
+    console.log("POST /user : ✓ Account managers verified");
+
+    // Check if the user already exists
+    const userQuery = supabase.from("user").select().ilike("account", accountAddress);
+    const { data: userData } = await userQuery;
+    if (userData) {
+      return new Response("Account already exists", {
+        status: 409,
+      });
+    }
+
+    console.log("POST /user : ✓ New user verified");
+
+    const fameishAccount = await publicClient.readContract({
+      address: process.env.FAMEISH_CONTRACT_ADDRESS! as `0x${string}`,
+      abi: fameishAbi,
+      functionName: "fameishAccount",
+    });
+
+    const isFollowing = await publicClient.multicall({
+      contracts: [
+        {
+          address: getContract("GraphImpl").address,
+          abi: graphApi,
+          functionName: "isFollowing",
+          args: [accountAddress, fameishAccount],
+        },
+        {
+          address: getContract("GraphImpl").address,
+          abi: graphApi,
+          functionName: "isFollowing",
+          args: [accountAddress, process.env.LENS_FAMEISH_ACCOUNT_ADDRESS! as `0x${string}`],
+        },
+      ],
+    });
+
+    // Check if the account is following the winning account
+    if (isFollowing[0].error || !isFollowing[0].result) {
+      return new Response("Not following winner", {
+        status: 401,
+      });
+    }
+
+    // Check if the account is the Fameish account
+    if (isFollowing[1].error || !isFollowing[1].result) {
+      return new Response("Not following Fameish account", {
+        status: 401,
+      });
+    }
+
+    console.log("POST /user : ✓ Following status verified");
+
+    // create a new user
+    const { data: insertData, error: insertError } = await supabase.from("user").insert({ account: accountAddress });
+    console.log("POST /user : insertData", insertData);
+
+    if (insertError) {
+      console.error("POST /user : insertError", insertError);
+      return new Response("Failed to create user", {
+        status: 500,
+      });
+    }
+
+    console.log("POST /user : ✓ Created user");
+
+    return Response.json({ success: true, user: insertData });
   } catch (e) {
+    console.error("POST /user error", e);
     return new Response("Unauthorized", {
       status: 401,
     });
   }
-
-  return Response.json({ success: true });
 }
---
+++ packages/web/src/lib/abis/fameish.ts
@@ -0,0 +1,467 @@
+export const fameishAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_accountManager",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_lensGraph",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_fameishRandomAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "InvalidAccountParams",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidConstructorParams",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "fameishAccount",
+        type: "address",
+      },
+    ],
+    name: "FameishAccountSet",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "randomIndex",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "followerListURI",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "followerCount",
+        type: "uint256",
+      },
+    ],
+    name: "RandomIndexSelected",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "previousAdminRole",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "newAdminRole",
+        type: "bytes32",
+      },
+    ],
+    name: "RoleAdminChanged",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+    ],
+    name: "RoleGranted",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "sender",
+        type: "address",
+      },
+    ],
+    name: "RoleRevoked",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "ACCOUNT_MANAGER",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "DEFAULT_ADMIN_ROLE",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address payable[]",
+        name: "_followers",
+        type: "address[]",
+      },
+    ],
+    name: "bulkFollow",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address payable[]",
+        name: "_followers",
+        type: "address[]",
+      },
+    ],
+    name: "bulkUnfollow",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fameishAccount",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fameishRandom",
+    outputs: [
+      {
+        internalType: "contract FameishRandom",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "followerCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "followerIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "followerListURI",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+    ],
+    name: "getRoleAdmin",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "grantRole",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "hasRole",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "lensGraph",
+    outputs: [
+      {
+        internalType: "contract Graph",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "renounceRole",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "role",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "revokeRole",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_followerCount",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_followerListURI",
+        type: "string",
+      },
+    ],
+    name: "selectRandom",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_fameishAccount",
+        type: "address",
+      },
+    ],
+    name: "setFameishAccount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "supportsInterface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "totalFollows",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "totalFollows",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "account",
+        type: "address",
+      },
+    ],
+    name: "totalUnfollows",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "totalUnfollows",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
