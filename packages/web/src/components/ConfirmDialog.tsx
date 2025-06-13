31827e507e0dfb6d74a52dfa0320678077cc5119
Paul Burke <paul@paulburke.co>
2025-06-13 09:48:19 -0400

    feat: adds logout confirm dialog

---
+++ packages/web/src/components/ConfirmDialog.tsx
@@ -0,0 +1,81 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { forwardRef, ReactNode, useImperativeHandle, useState } from "react";
+
+export type ConfirmDialogRef = {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
+};
+
+type ConfirmDialogProps = {
+  title: string;
+  description?: string;
+  confirmButtonText?: string;
+  cancelButtonText?: string;
+  onConfirm: () => Promise<void>;
+  onCancel?: () => Promise<void>;
+  children?: ReactNode;
+};
+
+const ConfirmDialog = forwardRef<ConfirmDialogRef, ConfirmDialogProps>(
+  (
+    { title, description, confirmButtonText = "Confirm", cancelButtonText = "Cancel", onConfirm, onCancel, children },
+    ref,
+  ) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleConfirm = async () => {
+      await onConfirm();
+      setIsOpen(false);
+    };
+
+    const handleCancel = async () => {
+      if (onCancel) {
+        await onCancel();
+      }
+      setIsOpen(false);
+    };
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        isOpen: isOpen,
+        open: () => setIsOpen(true),
+        close: () => setIsOpen(false),
+      }),
+      [isOpen],
+    );
+
+    return (
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            {description && <DialogDescription>{description}</DialogDescription>}
+          </DialogHeader>
+          {children}
+          <DialogFooter>
+            {onCancel && (
+              <Button type="button" variant="secondary" onClick={handleCancel}>
+                {cancelButtonText}
+              </Button>
+            )}
+            <Button type="button" variant="outline" onClick={handleConfirm}>
+              {confirmButtonText}
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    );
+  },
+);
+
+export default ConfirmDialog;
--- packages/web/src/components/Header.tsx
+++ packages/web/src/components/Header.tsx
@@ -7,6 +7,8 @@ import { useReadContract } from "wagmi";
 import { fameishAbi } from "@/lib/abis/fameish";
 import Image from "next/image";
 import { track } from "@vercel/analytics";
+import ConfirmDialog, { ConfirmDialogRef } from "@/components/ConfirmDialog";
+import { useRef } from "react";
 
 type HeaderProps = {
   showLinks?: boolean;
@@ -24,79 +26,91 @@ export default function Header({ showLinks, showAccount = true }: HeaderProps) {
     functionName: "winnerSetTimestamp",
   });
 
+  const confirmLogoutDialog = useRef<ConfirmDialogRef>(null);
+
   const handleLogout = async () => {
     await logOut();
     router.push("/");
   };
 
   return (
-    <header className="w-full px-6 py-4 grid grid-cols-2 md:grid-cols-[1fr_auto_1fr] items-center">
-      <Link href="/" className={`${!showLinks && "md:opacity-45"}`}>
-        <Image src="/images/fameish-logo.svg" alt="Fameish logo" width={1454} height={367} className="h-7 w-auto" />
-      </Link>
+    <>
+      <header className="w-full px-6 py-4 grid grid-cols-2 md:grid-cols-[1fr_auto_1fr] items-center">
+        <Link href="/" className={`${!showLinks && "md:opacity-45"}`}>
+          <Image src="/images/fameish-logo.svg" alt="Fameish logo" width={1454} height={367} className="h-7 w-auto" />
+        </Link>
 
-      {showLinks ? (
-        <nav className="hidden md:flex items-center space-x-8">
-          <Button
-            variant="link"
-            className="text-gray-700 hover:text-black p-0"
-            onClick={() => {
-              track("Click", { name: "How it works", location: "Header" });
-              router.push("/signup");
-            }}
-          >
-            How it works
-          </Button>
-          <Button
-            variant="link"
-            className="text-gray-700 hover:text-black p-0"
-            onClick={() => {
-              track("Click", { name: "Terms of service", location: "Header" });
-              router.push("/p/terms");
-            }}
-          >
-            Terms of service
-          </Button>
-        </nav>
-      ) : (
-        <div className="hidden md:block">
-          {drawingTimestamp !== undefined && drawingTimestamp > 0 && (
-            <AnimatedCountdownClock
-              textClassName="!text-xl opacity-65"
-              animationEnabled={false}
-              targetDate={new Date(Number(drawingTimestamp) * 1000 + 86400000)}
-              onComplete={() => alert("done!")}
-            />
-          )}
-        </div>
-      )}
-
-      {showAccount && (
-        <div className="flex items-center justify-end gap-x-6">
-          {lensUser ? (
-            showLinks ? (
-              <Link href="/account" className="font-medium">
-                Account
-              </Link>
-            ) : (
-              <Button variant="link" onClick={handleLogout} className="opacity-65 p-0">
-                Log out
-              </Button>
-            )
-          ) : (
-            showLinks && (
-              <Link href="/signup" className="font-medium">
-                Log in
-              </Link>
-            )
-          )}
-          {showLinks && !walletAddress && (
-            <Button onClick={() => router.push("/signup")} size="lg" className="hidden md:block font-medium">
-              Sign up
+        {showLinks ? (
+          <nav className="hidden md:flex items-center space-x-8">
+            <Button
+              variant="link"
+              className="text-gray-700 hover:text-black p-0"
+              onClick={() => {
+                track("Click", { name: "How it works", location: "Header" });
+                router.push("/signup");
+              }}
+            >
+              How it works
             </Button>
-          )}
+            <Button
+              variant="link"
+              className="text-gray-700 hover:text-black p-0"
+              onClick={() => {
+                track("Click", { name: "Terms of service", location: "Header" });
+                router.push("/p/terms");
+              }}
+            >
+              Terms of service
+            </Button>
+          </nav>
+        ) : (
+          <div className="hidden md:block">
+            {drawingTimestamp !== undefined && drawingTimestamp > 0 && (
+              <AnimatedCountdownClock
+                textClassName="!text-xl opacity-65"
+                animationEnabled={false}
+                targetDate={new Date(Number(drawingTimestamp) * 1000 + 86400000)}
+                onComplete={() => alert("done!")}
+              />
+            )}
+          </div>
+        )}
+
+        {showAccount && (
+          <div className="flex items-center justify-end gap-x-6">
+            {lensUser ? (
+              showLinks ? (
+                <Link href="/account" className="font-medium">
+                  Account
+                </Link>
+              ) : (
+                <Button variant="link" onClick={() => confirmLogoutDialog.current?.open()} className="opacity-65 p-0">
+                  Log out
+                </Button>
+              )
+            ) : (
+              showLinks && (
+                <Link href="/signup" className="font-medium">
+                  Log in
+                </Link>
+              )
+            )}
+            {showLinks && !walletAddress && (
+              <Button onClick={() => router.push("/signup")} size="lg" className="hidden md:block font-medium">
+                Sign up
+              </Button>
+            )}
+          </div>
+        )}
+      </header>
+      <ConfirmDialog ref={confirmLogoutDialog} title="Log out" confirmButtonText="Log out" onConfirm={handleLogout}>
+        <div className="flex flex-col gap-2">
+          <span>Are you sure you want to log out?</span>
+          <span className="text-xs opacity-65">
+            This will <strong>not</strong> affect your eligibility for future drawings.
+          </span>
         </div>
-      )}
-    </header>
+      </ConfirmDialog>
+    </>
   );
 }
